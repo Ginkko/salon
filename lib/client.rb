ec1b914b6e093fc9a757c8c050bb6b62bdce0135
Alec <Incubusalec@gmail.com>
2015-05-08 19:35:21 +0000

    Add spec for Client#save. Add Client#init, save, ==, and .all. Passes spec.

--- lib/client.rb
+++ lib/client.rb
@@ -1,2 +1,31 @@
 class Client
+  attr_reader :name, :id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  def self.all
+    clients = []
+    results = DB.exec("SELECT * FROM clients;")
+    results.each do |result|
+      name = result.fetch('name')
+      id = result.fetch('id').to_i
+      clients.push(Client.new({:name => name, :id => id}))
+    end
+    clients
+  end
+
+  def save
+    result = DB.exec("INSERT INTO clients (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first.fetch('id').to_i
+  end
+
+  def ==(other_client)
+    same_name = @name == other_client.name
+    same_id = @id == other_client.id
+    same_name && same_id
+  end
+
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,1 +1,13 @@
 require "spec_helper"
+
+describe(Client) do
+
+  describe ('#save') do
+    it('adds the client to the list of clients') do
+      test_client = Client.new({:name => "Khan", :id => nil})
+      test_client.save
+      expect(Client.all).to eq([test_client])
+    end
+  end
+
+end
