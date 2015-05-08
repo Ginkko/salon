4f4f44835df48713b0d75057c9269073df64ac98
Alec <Incubusalec@gmail.com>
2015-05-08 22:28:15 +0000

    Add integration spec for adding a client to a stylist. Passes spec.

--- app.rb
+++ app.rb
@@ -29,12 +29,34 @@ get('/stylists/new') do
   erb(:stylist_add)
 end
 
+get('/stylists/:id') do
+  stylist_id = params.fetch('id').to_i
+  @stylist = Stylist.find(stylist_id)
+  @clients = @stylist.clients
+  @all_clients = Client.all
+  erb(:stylist)
+end
+
+post('/stylists/:id') do
+  stylist_id = params.fetch('id').to_i
+  @stylist = Stylist.find(stylist_id)
+  client_id = params.fetch('clients').to_i
+  @stylist.client_add(client_id)
+  @all_clients = Client.all
+  erb(:stylist)
+end
+
+
 get('/clients') do
   @clients = Client.all
   erb(:clients)
 end
 
 post('/clients') do
+  name = params.fetch('name')
+  client = Client.new({:name => name, :id => nil, :stylist_id => nil})
+  client.save
+  @clients = Client.all
   erb(:clients)
 end
 
--- spec/a1_integration_spec.rb
+++ spec/a1_integration_spec.rb
@@ -13,3 +13,25 @@ describe('viewing and adding stylists', {:type => :feature}) do
     expect(page).to have_content("Tiberius")
   end
 end
+
+describe('viewing and adding clients', {:type => :feature}) do
+  it('processes user input to add a client to the client list') do
+    visit('/clients/new')
+    fill_in('name', :with => "Khan")
+    click_button('submit')
+    expect(page).to have_content("Khan")
+  end
+end
+
+describe('adding a client to a stylist', {:type => :feature}) do
+  it('shows a stylits details with a list of clients. On selecting a client and clicking submit, display added client on updated stylist list.') do
+    test_stylist = Stylist.new({:name => "Tiberius", :id => nil})
+    test_stylist.save
+    test_client = Client.new({:name => "Khan", :id => nil, :stylist_id => nil})
+    test_client.save
+    visit("/stylists/#{test_stylist.id}")
+    select test_client.name, from: "clients"
+    click_button('submit')
+    expect(page).to have_content(test_client.name)
+  end
+end
